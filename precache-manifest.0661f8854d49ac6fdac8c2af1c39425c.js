7ffe5798ca02941664a6a2454dfcd5bbd2b416b6
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-11-30 13:40:28 +0200

    Updates

---
+++ precache-manifest.0661f8854d49ac6fdac8c2af1c39425c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e18f247d8b44fbfc052cf2fe1960115",
+    "url": "/robofriends-react-app/index.html"
+  },
+  {
+    "revision": "02fba0a5237fe3cc17ee",
+    "url": "/robofriends-react-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d9c6ac1e0e7abb17c36d",
+    "url": "/robofriends-react-app/static/css/main.7a1812eb.chunk.css"
+  },
+  {
+    "revision": "02fba0a5237fe3cc17ee",
+    "url": "/robofriends-react-app/static/js/2.a390a847.chunk.js"
+  },
+  {
+    "revision": "d9c6ac1e0e7abb17c36d",
+    "url": "/robofriends-react-app/static/js/main.c7f4393a.chunk.js"
+  },
+  {
+    "revision": "187b745e8656c822d1de",
+    "url": "/robofriends-react-app/static/js/runtime-main.969872b2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
